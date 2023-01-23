f63a729896f8797aba15f9e70d6ca3e81d7ff068
Ozzy Domarkas <94358381+OzzyD1@users.noreply.github.com>
2023-01-23 11:30:30 +0000

    Initial upload

---
+++ sketch.js
@@ -0,0 +1,14 @@
+let ready = false;
+
+function setup() {
+    createCanvas(1200, 800);
+  }
+  
+  function draw() {
+  }
+
+  function mousepressed () {
+    if (ready == false){
+        ready = true;
+    }
+  }
